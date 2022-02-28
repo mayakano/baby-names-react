8dcfa7a8670c1eb4ce46541e1953a2c3ab9f3703
Maya Kano <mayakanoweb@gmail.com>
2022-02-27 20:03:34 -0500

    added useref and useffect

--- src/components/search.js
+++ src/components/search.js
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 export function Search() {
   const [searchValue, setSearchValue] = useState("");
+  const inputRef = useRef();
 
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
   function handleChange(event) {
     setSearchValue(event.target.value);
   }
@@ -10,6 +14,7 @@ export function Search() {
   return (
     <header>
       <input
+        ref={inputRef}
         type="text"
         placeholder="Type a name..."
         value={searchValue}
